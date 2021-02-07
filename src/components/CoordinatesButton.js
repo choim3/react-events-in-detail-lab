07f963044b675dc82ba2fe6900250595c687c775
choim3 <choim3@spu.edu>
2021-02-07 15:56:03 -0800

    done

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,15 @@
 // Code CoordinatesButton Component Here
+import React, {Component} from 'react'
+
+export default  class CoordinatesButton extends Component {
+  handleClick = (e) => {
+    let array = [e.clientX, e.clientY]
+    this.props.onRecieveCoordinates(arr)
+  }
+  
+  render() {
+    return(
+      <button onClick = {this.handleClick}> </button>
+    )
+  }
+}
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,15 @@
 // Code DelayedButton Component Here
+import React, {Component}, from 'react'
+
+export default class DelayedButton extends Component {
+  render() {
+    handleClick = (event) => {
+      event.persist()
+      setTimeout(() => this.props.onDelayedClick(event), this.props.delay)
+    }
+    return(
+      <button onClick = {this.handleClick}> </button>
+    )
+
+  }
+}
